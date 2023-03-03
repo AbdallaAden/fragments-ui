4077bad6aa4b916536d0ef8de62457c67642c81c
git-illite <71469298+git-illite@users.noreply.github.com>
2023-03-03 08:37:05 -0500

    updated display of single frag and multiple

--- src/app.js
+++ src/app.js
@@ -1,27 +1,24 @@
 // src/app.js
 
-import { Auth, getUser } from './auth';
-import { getUserFragments, postUserFragments, getFragmentById } from './api';
-
+import { Auth, getUser } from "./auth";
+import { getUserFragments, postUserFragments, getFragmentById } from "./api";
 
 async function init() {
   // Get our UI elements
-  const userSection = document.querySelector('#user');
-  const loginBtn = document.querySelector('#login');
-  const logoutBtn = document.querySelector('#logout');
- 
-  const fragType = document.getElementById('fragmentType')
-  const textFragmentBtn = document.querySelector('#textFragBtn')
-  const textFrag = document.querySelector('#textFrag')
-  const getFragsBtn =document.querySelector('#getFrags')
-  
-  const FragmentByIdBtn = document.querySelector('#getFragById')
-  const FragId = document.querySelector('#fragId')
+  const userSection = document.querySelector("#user");
+  const loginBtn = document.querySelector("#login");
+  const logoutBtn = document.querySelector("#logout");
 
-  const out1 = document.getElementById('#output1')
+  const fragType = document.getElementById("fragmentType");
+  const textFragmentBtn = document.querySelector("#textFragBtn");
+  const textFrag = document.querySelector("#textFrag");
+  const getFragsBtn = document.querySelector("#getFrags");
+
+  const FragmentByIdBtn = document.querySelector("#getFragById");
+  const FragId = document.querySelector("#fragId");
+
+  const out1 = document.getElementById("#output1");
   let gotID;
-
-
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
@@ -42,7 +39,7 @@ async function init() {
     logoutBtn.disabled = true;
     return;
   }
-  
+
   // Log the user info for debugging purposes
   console.log({ user });
 
@@ -50,7 +47,7 @@ async function init() {
   userSection.hidden = false;
 
   // Show the user's username
-  userSection.querySelector('.username').innerText = user.username;
+  userSection.querySelector(".username").innerText = user.username;
 
   // Disable the Login button
   loginBtn.disabled = true;
@@ -58,36 +55,102 @@ async function init() {
   // Do an authenticated request to the fragments API server and log the result
   //getUserFragments(user)
   textFragmentBtn.onclick = () => {
-    console.log('frag type ' + fragType.value)
-    postUserFragments(user,textFrag.value,fragType.value);
-   }
-   getFragsBtn.onclick = async () => {
+    console.log("frag type " + fragType.value);
+    postUserFragments(user, textFrag.value, fragType.value);
+  };
+
+  getFragsBtn.onclick = async () => {
     gotID = await getUserFragments(user);
-    myDisplayer(gotID)
+    myDisplayer(gotID);
     //document.getElementById("output1").innerHTML = JSON.stringify(gotID,null, "\t");
-   }
-   FragmentByIdBtn.onclick = async () => {
-    if(!FragId || FragId.value =="")  alert("Fragment ID section can't be empty")
-    else{
-    gotID = await getFragmentById(user,FragId.value)
-    console.log(gotID.data.fragment)
-    //gotID = gotID.data.fragment
-   //myDisplayer(gotID)
-   document.getElementById("output1").innerHTML = JSON.stringify(gotID.data.fragment,null, "\t");
-   }
-  }
-  async function myDisplayer(some) {
-    console.log("some data : "+ some.fragments.length)
-    for (const key in some) {
-      if (Object.hasOwnProperty.call(some, key)) {
-        console.log(`${key}: ${some[key]}`);
-        document.getElementById("output1").innerHTML = JSON.stringify(some[key],null, "\t");
-      }
+  };
+
+  FragmentByIdBtn.onclick = async () => {
+    if (!FragId || FragId.value == "")
+      alert("Fragment ID section can't be empty");
+    else {
+      gotID = await getFragmentById(user, FragId.value);
+      console.log(gotID.data.fragment);
+      myDisplayer(gotID);
+      //document.getElementById("output1").innerHTML = JSON.stringify(gotID.data.fragment,null, "\t");
     }
-    //
+  };
+
+  function myDisplayer(some) {
+    console.log(Object.keys(some).length);
+    document.getElementById("output1").innerHTML = "";
+    const table = document.createElement("table");
+    table.border = "1";
+    table.style.borderCollapse = "collapse";
+    table.style.background = "darkblue";
+
+    if (Object.keys(some).length == 1) {
+      some1 = some.data.fragment;
+      for (const key in some1) {
+        if (Object.hasOwnProperty.call(some1, key)) {
+          const row = table.insertRow();
+          const keyCell = row.insertCell();
+          const valueCell = row.insertCell();
+          keyCell.innerText = key;
+          valueCell.innerText = some1[key];
+          row.classList.add("fragment-row");
+        }
+      }
+    } else {
+      some.fragments.forEach((fragment) => {
+        for (const key in fragment) {
+          if (Object.hasOwnProperty.call(fragment, key)) {
+            const dataRow = table.insertRow();
+            const headerCell = dataRow.insertCell();
+            headerCell.innerText = key;
+            headerCell.style.fontWeight = "bold";
+            headerCell.style.padding = "8px";
+            const dataCell = dataRow.insertCell();
+            dataCell.innerText = fragment[key];
+            dataCell.style.padding = "8px";
+          }
+        }
+        // Create an empty row after the 'size' row
+        const emptyRow = table.insertRow();
+        const emptyCell = emptyRow.insertCell();
+        emptyCell.classList.add("empty-cell");
+        emptyCell.colSpan = Object.keys(some.fragments[0]).length;
+
+        if (Object.keys(fragment).length === 0) {
+          const emptyCell = dataRow.insertCell();
+          emptyCell.classList.add("empty-cell");
+        }
+      });
+    }
+    // Display table
+    document.getElementById("output1").innerHTML = "";
+    document.getElementById("output1").appendChild(table);
   }
 
+  /*function myDisplayerFrag(fragment) {
+    const table = document.createElement("table");
+    table.border = "1";
+    table.style.borderCollapse = "collapse";
+    table.style.background = "darkblue";
+
+    // Create table data
+
+    for (const key in fragment) {
+      if (Object.hasOwnProperty.call(fragment, key)) {
+        const row = table.insertRow();
+        const keyCell = row.insertCell();
+        const valueCell = row.insertCell();
+        keyCell.innerText = key;
+        valueCell.innerText = fragment[key];
+        row.classList.add("fragment-row");
+      }
+    }
+
+    // Display table
+    document.getElementById("output1").innerHTML = "";
+    document.getElementById("output1").appendChild(table);
+  }*/
 }
 
 // Wait for the DOM to be ready, then start the app
-addEventListener('DOMContentLoaded', init);
+addEventListener("DOMContentLoaded", init);
